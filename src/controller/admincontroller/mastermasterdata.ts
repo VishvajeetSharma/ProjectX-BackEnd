ffe03ea3931f563253f0ee7fc5d77697560261c9
Vishvajeet Sharma <vsharma6705@gmail.com>
2026-03-24 09:36:47 +0530

    master course api

--- src/controller/admincontroller/mastermasterdata.ts
+++ src/controller/admincontroller/mastermasterdata.ts
@@ -1,8 +1,10 @@
+import { mastercourse } from "../../entities/mastercourse";
 import { masterplan } from "../../entities/masterplan";
 import { createResponse } from "../../helpers/createResponse";
 
 export const createmasterplan = async (req: any, res: any) => {
   try {
+    
     const { name, desc, credit, price, offer, duration, is_rec, status } =   req.body;
     const isExist = await masterplan.findOne({ where: { name } });
     if(isExist){
@@ -11,8 +13,8 @@ export const createmasterplan = async (req: any, res: any) => {
       const result= await masterplan.save( { name, desc, credit, price, offer, duration, is_rec, status } )
        return createResponse(res, true, 200, "Plan created successfully",result, false);
     }
-  } catch (error) {
-    return createResponse(res, false, 500, "Internal Server Error", [], true);
+  } catch (error: any) {
+    return createResponse(res, false, 500, error.message || "Internal Server Error", [], true);
   }
 };
 export const getmasterplan = async (req: any, res: any) => {
@@ -21,5 +23,22 @@ export const getmasterplan = async (req: any, res: any) => {
        return createResponse(res, true, 200, "Plan fetched successfully",result, false);
     } catch (error) {
     return createResponse(res, false, 500, "Internal Server Error", [], true);
+  }
+};
+
+export const createmastercourse = async (req: any, res: any) => {
+  try {
+    const {thumbnail, content} = req.files;
+    const {title, desc, level, rating, duration, type, status} =   req.body;
+    // Login for file upload
+
+    const isExist = await mastercourse.findOne({ where: { title } });
+    if(isExist){
+      return createResponse(res, false, 400, "Course already exists", [], true);
+    }else{
+     // login here
+    }
+  } catch (error: any) {
+    return createResponse(res, false, 500, error.message || "Internal Server Error", [], true);
   }
 };
--- src/routes/admin/adminrouter.ts
+++ src/routes/admin/adminrouter.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { adminLogin, adminRegister } from '../../controller/admincontroller/adminauthcontroller';
-import { createmasterplan, getmasterplan } from '../../controller/admincontroller/mastermasterdata';
+import { createmastercourse, createmasterplan, getmasterplan } from '../../controller/admincontroller/mastermasterdata';
 const adminRouter=express.Router();
 
 adminRouter.post("/register", adminRegister)
@@ -9,4 +9,8 @@ adminRouter.post("/login", adminLogin)
 // master plan
 adminRouter.post("/create-master-plan", createmasterplan) 
 adminRouter.get("/get-master-plan", getmasterplan) 
+
+// master course
+adminRouter.post("/create-master-course", createmastercourse) 
+adminRouter.get("/get-master-course", getmasterplan) 
 export default adminRouter;
