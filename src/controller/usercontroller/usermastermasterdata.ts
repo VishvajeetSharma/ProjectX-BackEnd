72d2b77502f5b1045502d4fe3f1c696513c15b02
Vishvajeet Sharma <vsharma6705@gmail.com>
2026-03-26 09:38:30 +0530

    new

--- src/controller/usercontroller/usermastermasterdata.ts
+++ src/controller/usercontroller/usermastermasterdata.ts
@@ -1,5 +1,6 @@
 import { mastercourse } from "../../entities/mastercourse";
 import { masterplan } from "../../entities/masterplan";
+import { plan } from "../../entities/plan";
 import { users } from "../../entities/user";
 import { createResponse } from "../../helpers/createResponse";
  
@@ -12,6 +13,21 @@ export const usergetmasterplan = async (req: any, res: any) => {
   } catch (error: any) {
     return createResponse(res, false, 500, error.message || "Internal Server Error", [], true);
   }
+}; 
+
+export const userPurchaseplan = async (req: any, res: any) => {
+  try {
+    const {user_id,plan_id}=req.body 
+    await  plan.save({user_id,plan_id})
+   const masterplanRes = await masterplan.findOne({  where:{id:plan_id}});
+   const userRes=await users.findOne({where:{id:user_id}})
+   const finalCredit:any=parseInt(masterplanRes?.credit)+parseInt(userRes?.credit);
+    await users.update({id:user_id},{credit:finalCredit})
+    return createResponse(res, true, 200, "Plans fetched successfully", finalCredit, false);
+  } catch (error: any) {
+    return createResponse(res, false, 500, error.message || "Internal Server Error", [], true);
+  }
+  
 }; 
 // Master Course 
   
--- src/routes/user/userrouter.ts
+++ src/routes/user/userrouter.ts
@@ -2,10 +2,11 @@ import express from "express";
 import { userLogin, userRegister } from "../../controller/usercontroller/userauthcontroller";
 import { validateMiddleware } from "../../middleware/validationMiddleware";
 import { verifyToken } from "../../middleware/authMiddleware";
-import { usergetmasterplan } from "../../controller/usercontroller/usermastermasterdata";
+import { usergetmasterplan, userPurchaseplan } from "../../controller/usercontroller/usermastermasterdata";
 const userRouter = express.Router();
 userRouter.post("/register", validateMiddleware, userRegister);  
 userRouter.post("/login", validateMiddleware, userLogin);
 
 userRouter.get("/user-master-plan",verifyToken,  usergetmasterplan); 
+userRouter.post("/user-purchase-plan",userPurchaseplan); 
 export default userRouter;
