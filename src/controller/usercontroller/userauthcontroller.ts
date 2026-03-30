51e3d90ee3f05a085d8f7e97abb6a4a4876b4d56
Vishvajeet Sharma <vsharma6705@gmail.com>
2026-03-30 09:26:38 +0530

    new

--- src/controller/usercontroller/userauthcontroller.ts
+++ src/controller/usercontroller/userauthcontroller.ts
@@ -3,7 +3,7 @@ import "dotenv/config";
 import { createResponse } from "../../helpers/createResponse";
 import bcrypt from "bcrypt";
 import { generateToken } from "../../helpers/jwt";
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 export const userRegister = async (req: any, res: any) => {
   try {
     const { name, email, password = "Test@12345", mobile } = req.body;
@@ -67,38 +67,68 @@ export const userLogin = async (req: any, res: any) => {
   }
 };
 
+const generatePassword = () => {
+  let pass = "";
+  let str = "A1234B569!*&^#%#CDE7543FGHIJKLMNOPQRSTUVWXYZ233%%909";
+  for (let i = 0; i <= 8; i++) {
+    const randIndex = Math.ceil(Math.random() * 10);
+    pass += str[randIndex];
+  }
+  return pass;
+};
 export const userForgetPassword = async (req: any, res: any) => {
   const { email } = req.body;
   try {
-    let mailTransporter =
-    nodemailer.createTransport(
-        {
-            service: 'gmail',
-            auth: {
-                user: 'vsharma6705@gmail.com',
-                pass: 'hljx cwze xfqn vqyg'
-            }
+    const isExists = await users.findOne({ where: { email } });
+    if (!isExists) {
+      return createResponse(res, false, 404, "User Not Found", [], true);
+    } else {
+      let password = generatePassword();
+
+      let mailTransporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: "vsharma6705@gmail.com",
+          pass: "hljx cwze xfqn vqyg",
+        },
+      });
+
+      let mailDetails = {
+        from: "vsharma6705@gmail.com",
+        to: `${email}`,
+        subject: "Test mail",
+        text: "",
+        html: `<html>
+        <head>
+            <style>
+                body { font-family: sans-serif; }
+                .container { padding: 20px; background-color: #f5f7fa; }
+                h1 { color: #333; }
+            </style>
+        </head>
+        <body>
+            <div class="container">
+                <h1>Welcome, User!</h1>
+                <p>Your Password id : ${password}</p>
+                <a href="https://example.com" style="background-color: #007bff; color: white; padding: 10px 20px; text-decoration: none; border-radius: 5px;">
+                    Visit our website
+                </a>
+            </div>
+        </body>
+        </html>`,
+      };
+
+      await mailTransporter.sendMail(mailDetails, function (err, data) {
+        if (err) {
+          console.log("Error Occurs");
+        } else {
+          console.log("Email sent successfully");
         }
-    );
-
-let mailDetails = {
-    from: 'vsharma6705@gmail.com',
-    to: 'ravilms@yopmail.com',
-    subject: 'Test mail',
-    text: 'Node.js testing mail for GeeksforGeeks'
-};
-
-await mailTransporter
-    .sendMail(mailDetails,
-        function (err, data) {
-            if (err) {
-                console.log('Error Occurs');
-            } else {
-                console.log('Email sent successfully');
-            }
-        });
-        
-    return createResponse(res, true, 200, "okkk", { email }, false);
+      });
+      const hashedPassword: any = await bcrypt.hash(password, 10);
+    const result=  await users.update({ email }, { password: hashedPassword });
+      return createResponse(res, true, 200, "Password Updated succesfully , Please check your email and login.",result, false);
+    }
   } catch (error) {
     return createResponse(res, false, 500, "Internal Server Error", [], true);
   }
